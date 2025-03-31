6a758b7290f579de93d933b605dcd0b95a40d924
PUP04ka2014 <PUP04kaDanil@gmail.com>
2025-03-31 23:39:27 +0800

    xz
    1.1.0

--- server2.js
+++ server2.js
@@ -1,36 +1,18 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-var mysql = require('mysql2');
-const sessions = require('express-session');
-var parseUrl = require('body-parser');
-
-app.use(sessions({
-	secret: "thisismysecrctekey",
-	saveUninitialized:true,
-	cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 24 hours
-	resave: false
-}));
-
-var con = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "root", 
-	database: "test"
-});
+app.use(express.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname));
-let encodeUrl = parseUrl.urlencoded({ extended: false });
 
-app.use('/', (require, response) => {
-    response.render('test')
+app.use('', (req, res) => {
+    res.render('test')
 })
 
-app.get('/s',function(request, response){
-    response.render('reg')
+app.get('/s',function(req, res){
+	console.log('1')
 })
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+app.listen(3000, () => {
+    console.log(`Server is running at http://localhost:${3000}`);
 });
